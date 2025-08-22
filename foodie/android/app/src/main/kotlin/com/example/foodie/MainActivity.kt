af9a0db1e60630333f7228a8a5abd52a44178fb0
mrrajamiaraj <116349287+mrrajamiaraj@users.noreply.github.com>
2025-08-22 18:07:23 +0600

    raja

--- foodie/android/app/src/main/kotlin/com/example/foodie/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.foodie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- foodie/lib/app/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static ThemeData light = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF7A1A)),
-    scaffoldBackgroundColor: Colors.white,
-    useMaterial3: true,
-  );
-}
