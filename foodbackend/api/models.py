be18fa20c7255971fee6fc22d7bac93c6e6ff00b
mrrajamiaraj <116349287+mrrajamiaraj@users.noreply.github.com>
2025-08-23 02:24:25 +0600

    raja

--- foodbackend/api/models.py
+++ foodbackend/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    # A restaurant
+    name = models.CharField(max_length=120)
+    address = models.CharField(max_length=255, blank=True)
+    image_url = models.URLField(blank=True)
